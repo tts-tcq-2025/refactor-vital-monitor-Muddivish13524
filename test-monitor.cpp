b5b8c22e563a3b8c2181e671fb4ab868a787521c
Muddivish13524 <vishwanathmuddi@gmail.com>
2025-08-05 09:23:49 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -5,3 +5,11 @@ TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
   ASSERT_FALSE(vitalsOk(99, 102, 70));
   ASSERT_TRUE(vitalsOk(98.1, 70, 98));
 }
+
+
+assert(isTemperatureCritical(103.5f) == true);
+assert(isTemperatureCritical(98.6f) == false);
+assert(isPulseRateOutOfRange(45.0f) == true);
+assert(isSpo2Low(85.0f) == true);
+
+
