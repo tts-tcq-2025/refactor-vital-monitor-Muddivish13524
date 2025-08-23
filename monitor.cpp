125c7704e75af18e1f793c961e350c1cddaed8d7
Muddivish13524 <vishwanathmuddi@gmail.com>
2025-08-23 13:50:52 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -2,53 +2,9 @@
 #include <thread>
 #include <chrono>
 #include <string>
-#include "./monitor.h"
-
-using std::cout, std::flu#include <iostream>
-#include <thread>
-#include <chrono>
-#include <string>
-#include "./monitor.h"
+#include "./monitor.hpp"
 
 using std::cout, std::flush;
-using std::this_thread::sleep_for;
-using std::chrono::seconds;
-
-bool checkVital(const VitalCheck& vital, std::function<void(const std::string&)> alert);
-
-void PrintAlertMessage(const std::string& message) {
-    cout << message << "\n";
-    for (int i = 0; i < 6; ++i) {
-        cout << "\r* " << flush;
-        sleep_for(seconds(1));
-        cout << "\r *" << flush;
-        sleep_for(seconds(1));
-    }
-}
-
-bool checkVital(const VitalCheck& vital, std::function<void(const std::string&)> alert) {
-    if (vital.value < vital.min || vital.value > vital.max) {
-        alert(vital.name + " is out of range!");
-        return false;
-    }
-    return true;
-}
-
-int areAllVitalsNormal(float temperature, float pulseRate, float spo2,
-             std::function<void(const std::string&)> alert) {
-const VitalCheck vitals[] = {
-        {"Temperature", temperature, 95.0, 102.0},
-        {"Pulse Rate", pulseRate, 60.0, 100.0},
-        {"Oxygen Saturation", spo2, 90.0, 100.0}
-    };
-
-    bool allVitalsOk = true;
-    for (int i = 0; i < 3; ++i) {
-        allVitalsOk = checkVital(vitals[i], alert) && allVitalsOk;
-    }
-
-    return allVitalsOk;
-}sh;
 using std::this_thread::sleep_for;
 using std::chrono::seconds;
 
