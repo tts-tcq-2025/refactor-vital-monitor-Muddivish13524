92a1a55e9d19bf11cd560062cc2ff8b4194c8205
Muddivish13524 <vishwanathmuddi@gmail.com>
2025-08-25 10:31:39 +0530

    Update and rename language_selector.hpp to src/language_selector.hpp

--- language_selector.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-#include "types.hpp"
-
-// Localized user message for a classified vital
-std::string Message(const ClassifiedVital& cv);
-
-void setLanguage(Language lang);
-Language getLanguage();
